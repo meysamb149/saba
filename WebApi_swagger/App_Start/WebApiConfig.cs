233eade24fbc5cd942320d380a7abed4139a3ef6
meysam <meysam@STATISTIC>
2018-07-08 13:10:31 +0430

    Add project files.

---
+++ WebApi_swagger/App_Start/WebApiConfig.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web.Http;
+using Microsoft.Owin.Security.OAuth;
+using Newtonsoft.Json.Serialization;
+
+namespace WebApi_swagger
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+            // Configure Web API to use only bearer token authentication.
+            config.SuppressDefaultHostAuthentication();
+            config.Filters.Add(new HostAuthenticationFilter(OAuthDefaults.AuthenticationType));
+
+            
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            GlobalConfiguration.Configuration.Formatters.JsonFormatter.MediaTypeMappings
+            .Add(new System.Net.Http.Formatting.RequestHeaderMapping("Accept",
+                                          "text/html",
+                                          StringComparison.InvariantCultureIgnoreCase,
+                                          true,
+                                          "application/json"));
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+            
+        }
+    }
+}
---
+++ WebApi_swagger/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace WebApi_swagger.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
